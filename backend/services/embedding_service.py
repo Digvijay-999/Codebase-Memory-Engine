d82dd83b7b3b733fe08ed0149772fee594e4207c
Digvijay <dypatil2007@gmail.com>
2026-06-27 19:59:01 +0530

    added chunking and embedding

--- backend/services/embedding_service.py
+++ backend/services/embedding_service.py
@@ -1,3 +1,12 @@
 from sentence_transformers import SentenceTransformer
 
+# Load the model once when the app starts
 model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+def create_embedding(text: str):
+    """
+    Convert text into a vector embedding.
+    """
+    embedding = model.encode(text)
+    return embedding.tolist()
